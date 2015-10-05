b0b4bc9bc732f778b8111a3e92e5e243badb7523
abeljumanuel <abeljumanuel.rojas@gmail.com>
2015-10-05 01:27:51 -0500

    Update SueldoOperario.java
    hora trabajada

--- Ejercicios/src/ejercicios/SueldoOperario.java
+++ Ejercicios/src/ejercicios/SueldoOperario.java
@@ -7,7 +7,7 @@ public class SueldoOperario {
         float costoHora;
         float sueldo;
         horasTrabajadas = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese la cantidad de horas:"));
-        costoHora=Float.parseFloat(JOptionPane.showInputDialog(null, "Ingrese el valor de la hora:"));
+        costoHora=Float.parseFloat(JOptionPane.showInputDialog(null, "Ingrese el valor de la hora trabajada:"));
         sueldo = horasTrabajadas * costoHora;
         System.out.print("El empleado debe cobrar:"+sueldo);
     }
